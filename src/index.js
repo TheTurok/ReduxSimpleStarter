beb3589ae02954043e1895a4f12bdcc8d7346077
Timothy Choi <TurokChoi@gmail.com>
2018-08-07 14:02:02 -0700

    comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
+import React from 'react'; //used to create and manage components
+import ReactDom from 'react-dom'; //interact with actual DOM
+// DOM == Document Object Model
 const App = function(){
   return <div> Hi! </div>;
 }
