80ece6fc240931bbada7b6d3824bc5994135f588
Timothy Choi <TurokChoi@gmail.com>
2018-08-07 14:53:27 -0700

    files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'; //used to create and manage components
-import ReactDom from 'react-dom'; //interact with actual DOM
+import ReactDOM from 'react-dom'; //interact with actual DOM
 // DOM == Document Object Model
-const App = function(){
+const App = () => {
   return <div> Hi! </div>;
 }
+//component is function or objext that spits html
+ReactDOM.render(<App />, document.querySelector('.container')); //mkaing a jsx tag creats an instance
+  //make use of component, wrap it arounf jsx tag
 
-ReactDOM.render(app);
+
+  //one component per file 
