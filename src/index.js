e51155d548476e0cca4369fd3d794f669655412e
Timothy Choi <TurokChoi@gmail.com>
2018-08-08 15:28:35 -0700

    got input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react'; //used to create and manage components
 import ReactDOM from 'react-dom'; //interact with actual DOM
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyClMcNtXNHe5kpeSb-PbNgxx5da4JXz-bc'; //youtube api
+
 // DOM == Document Object Model
 const App = () => {
-  return <div> Hi! </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+ );
 }
 //component is function or objext that spits html
 ReactDOM.render(<App />, document.querySelector('.container')); //mkaing a jsx tag creats an instance
   //make use of component, wrap it arounf jsx tag
 
 
-  //one component per file 
+  //one component per file
