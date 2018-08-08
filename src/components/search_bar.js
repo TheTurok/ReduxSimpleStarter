b5d0697468485dbd138b43f38e67302c2fb0f93c
Timothy Choi <TurokChoi@gmail.com>
2018-08-08 16:28:02 -0700

    finished section

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input />; //React.createElement
-};
+class SearchBar extends Component { //have additional functionality when class component
+
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+
+  render() { //syntax to define mthod in class
+    //have to return jsx on render method
+    return (
+      <div>
+        <input
+        value = {this.state.term}
+        onChange = {event => this.setState({term: event.target.value})} />
+      </div>
+    );
+
+
+  }  //onChange react define property
+
+
+}
+
 
 
 export default SearchBar;
