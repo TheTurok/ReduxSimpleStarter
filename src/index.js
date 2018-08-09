20eb30b8b61ee9c9c3ba73050bc21b0238abb810
Timothy Choi <TurokChoi@gmail.com>
2018-08-08 17:52:40 -0700

    search bare fleshing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react'; //used to create and manage components
+import React, {Component} from 'react'; //used to create and manage components
 import ReactDOM from 'react-dom'; //interact with actual DOM
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyClMcNtXNHe5kpeSb-PbNgxx5da4JXz-bc'; //youtube api
 
 // DOM == Document Object Model
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
- );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 //component is function or objext that spits html
 ReactDOM.render(<App />, document.querySelector('.container')); //mkaing a jsx tag creats an instance
