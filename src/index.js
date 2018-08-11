bc9f1600b1d39319f9269faee0cbd0b5679e5d58
Timothy Choi <TurokChoi@gmail.com>
2018-08-10 17:03:32 -0700

    fleshing out files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'; //used to create and manage components
 import ReactDOM from 'react-dom'; //interact with actual DOM
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyClMcNtXNHe5kpeSb-PbNgxx5da4JXz-bc'; //youtube api
 
@@ -10,18 +12,27 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
-
   }
 
   render(){
     return (
       <div>
         <SearchBar />
+        <VideoDetail video = {this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo}) }
+          videos = {this.state.videos} />
       </div>
     );
   }
